19243dcb301b6a4e3ba334314a1abe354d2fe4fb
Franki09 <fgomezrosell@gmail.com>
2025-08-17 13:09:57 -0300

    Configuracion del proyecto para que funcione con next y tailwind, completa.

---
+++ src/app/components/BlogCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function BlogCard() {
+  return <div className="bg-blue-500">Esto es la blogCard</div>;
+}
+
+{
+  /* <div class="attribution">
+  Challenge by{" "}
+  <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+    Frontend Mentor
+  </a>
+  . Coded by <a href="#">Your Name Here</a>.
+</div>; */
+}
