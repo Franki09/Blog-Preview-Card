8990630d99032c20e4dcca2453897f5d73a2bdaf
Franki09 <fgomezrosell@gmail.com>
2025-08-18 18:15:06 -0300

    Maquetacion del componente terminado

--- src/app/components/BlogCard.jsx
+++ src/app/components/BlogCard.jsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
 import React from "react";
 
 export default function BlogCard() {
-  return <div className="bg-blue-500">Esto es la blogCard</div>;
+  return (
+    <>
+      <div>
+        <Image src={"/images/illustration-article.svg"} width={336} height={200} alt={"Blog Image"} />
+        <h2>Learning</h2>
+        <h3>Published 21 Dec 2023</h3>
+        <h1>HTML & CSS foundations</h1>
+        <p>These languages are the backbone of every website, defining structure, content, and presentation.</p>
+      </div>
+      <div className="flex">
+        <Image src={"/images/image-avatar.webp"} width={32} height={32} alt="avatar" />
+        <h2>Greg Hooper</h2>
+      </div>
+    </>
+  );
 }
 
 {
